70228c281a9efe6a7a402470139747326bd8fe13
Kyle Illenden <130949210+killenden@users.noreply.github.com>
2024-09-13 23:09:05 -0400

    Update GitHubActions.py

--- GitHubActions.py
+++ GitHubActions.py
@@ -1,10 +1,26 @@
 import sys
+import stats
  
 def main():
     if len(sys.argv) > 1:
-        db_number = sys.argv[1]
-        print(f"DB number received: {db_number}")
-        # You can now use db_number in your script
+        db_name = sys.argv[1]
+        week_number = sys.argv[2]
+        print(f"DB number received: {db_name}")
+        print(f"week number received: {week_number}")
+        stats.Team_Attempts(db_name)
+        stats.Team_Attempts_Pct(db_name)
+        stats.Team_Attempts_Both(db_name)
+        stats.Team_Attempts_Both_Pct(db_name)
+        stats.Target_Share(db_name)
+        stats.TPG_vs_YPR(db_name, weeks)
+        stats.RPG_YPG(db_name, weeks)
+        stats.TPG_RPG(db_name, weeks)
+        stats.RPG_vs_TDPR(db_name, weeks)
+        stats.RB_YPG_vs_TDPG(db_name, weeks)
+        stats.RB_YPG(db_name, weeks)
+        stats.Top12QB(db_name, weeks)
+        stats.Top12QB_1(db_name)
+        
     else:
         print("No DB number provided.")
 
