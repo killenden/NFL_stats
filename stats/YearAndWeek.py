cd2e34c5213ea0fc78e8469d1b3d27fecf146d16
Kyle Illenden <130949210+killenden@users.noreply.github.com>
2025-08-31 21:41:50 -0400

    Update YearAndWeek.py

--- stats/YearAndWeek.py
+++ stats/YearAndWeek.py
@@ -12,6 +12,9 @@ def SetValue():
         year = datetime.now().year
         weeks = 1
         print(f"Sleeper season type is not regular, defaulting to year: {year} and weeks: {weeks}")
+
+    print(f"year={year}")
+    print(f"weeks={weeks}")
         
 
 if __name__ == "__main__":
