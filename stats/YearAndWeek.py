7b3078e0fbabf5b41c1651b51543da32a3648903
killenden <soccerdogki@gmail.com>
2025-09-25 20:58:12 -0400

    Updated necessary files to get this working

--- stats/YearAndWeek.py
+++ stats/YearAndWeek.py
@@ -17,5 +17,22 @@ def SetValue():
     print(f"weeks={weeks}")
         
 
+def SetValueReturn():
+    nfl_state = SleeperInfo.get_nfl_state()
+    if nfl_state['season_type'] == 'regular':
+        year = nfl_state['season']
+        weeks = nfl_state['week']
+        print(f"Sleeper year received: {year}")
+        print(f"Sleeper week received: {weeks}")
+    else:
+        year = datetime.now().year
+        weeks = 1
+        print(f"Sleeper season type is not regular, defaulting to year: {year} and weeks: {weeks}")
+
+    print(f"year={year}")
+    print(f"weeks={weeks}")
+
+    return year, weeks
+
 if __name__ == "__main__":
     SetValue()
